871f9aa8126edc541e653004e9d19d623c3b5b1f
moosaad <mahmoud.said.radwan@gmail.com>
2021-09-12 22:06:54 +0200

    Commenting some lines of code

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ import { fetchBySiret } from './service/entreprise-infos-service.js';
 
 const app = express();
 
+// The only and main path for a given siren.
 app.get('/:siren', async (req, res) => {
 	try {
 		const data = await fetchBySiret(req.params.siren);
+		// Storing a siren, name and address of a given company in a JSON format.
 		let infos = {
 			siren: data.unite_legale.siren,
 			nom: data.unite_legale.denomination,
@@ -13,10 +15,12 @@ app.get('/:siren', async (req, res) => {
 		};
 		res.send(infos);
 	} catch (error) {
+		// Catches a Not-found error if provided siren is wrong!
 		res.status(404).send(error.message);
 	}
 })
 
+// The port left to be defined in the environment, or it's running on 3000
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost: ${port}...`)
--- service/entreprise-infos-service.js
+++ service/entreprise-infos-service.js
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch';
 
+// The basic URL of requesting and fetching information with a given siren.
 const API_URL = 'https://entreprise.data.gouv.fr/api/sirene/v3/unites_legales/';
 export const fetchBySiret = siren => {
 	let url = API_URL + siren;
